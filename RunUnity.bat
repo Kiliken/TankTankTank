6b8e3ce77827ecd04241f0eef35faf3b6c7d5d55
Kiliken <francescopaolomariani7@gmail.com>
2025-05-31 17:21:10 +0900

    RunUnity Updated

--- RunUnity.bat
+++ RunUnity.bat
@@ -1,5 +1,7 @@
 @echo off
 
 if exist "C:\Program Files\Unity\Hub\Editor\2022.3.56f1\Editor\Unity.exe" set unityPath="C:\Program Files\Unity\Hub\Editor\2022.3.56f1\Editor\Unity.exe"
+if exist "D:\Program Files\Unity\Editor\2022.3.56f1\Editor\Unity.exe" set unityPath="D:\Program Files\Unity\Editor\2022.3.56f1\Editor\Unity.exe"
+
 
 %unityPath% -projectPath %cd%
