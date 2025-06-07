286b264661b036c85cf813945721c058ec98fadc
rimu09 <168046166+rimu09@users.noreply.github.com>
2025-06-07 18:47:12 +0900

    ammo scripts

--- Assets/Scripts/AmmoBarrel.cs
+++ Assets/Scripts/AmmoBarrel.cs
@@ -5,14 +5,9 @@ using UnityEngine;
 public class AmmoBarrel : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void ()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
