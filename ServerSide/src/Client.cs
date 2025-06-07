3352dc3346d71864e6999fc5c3dd201265334c22
Kiliken <francescopaolomariani7@gmail.com>
2025-06-07 10:35:41 +0900

    New UDP Client/Server

--- ServerSide/src/Client.cs
+++ ServerSide/src/Client.cs
@@ -1,123 +1,51 @@
-// A C# program for Client
+// Client-Side Implementation Of UDP:
 using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-namespace Client {
-
-class Program {
-
-// Main Method
-static void Main(string[] args)
+class StudentUDPClient
 {
-    ExecuteClient();
-}
-
-// ExecuteClient() Method
-static void ExecuteClient()
-{
-
-    try {
+    public static void Main(string[] args)
+    {
+        UdpClient udpc = new UdpClient("127.0.0.1", 25565);
+        IPEndPoint ep = null;
         
-        // Establish the remote endpoint 
-        // for the socket. This example 
-        // uses port 11111 on the local 
-        // computer.
-        IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
-        IPAddress ipAddr = ipHost.AddressList[0];
-        IPAddress myIp = IPAddress.Parse("127.0.0.1");
-        IPEndPoint localEndPoint = new IPEndPoint(myIp, 25565);
-
-        // Creation TCP/IP Socket using 
-        // Socket Class Constructor
-        Socket sender = new Socket(myIp.AddressFamily,
-                   SocketType.Stream, ProtocolType.Tcp);
-
-        try {
-            
-            // Connect Socket to the remote 
-            // endpoint using method Connect()
-            sender.Connect(localEndPoint);
-
-            // We print EndPoint information 
-            // that we are connected
-            Console.WriteLine("Socket connected to -> {0} ",
-                          sender.RemoteEndPoint.ToString());
-
-            // Creation of message that
-            // we will send to Server
-            byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
-            int byteSent = sender.Send(messageSent);
-            
-        ConsoleKeyInfo cki;
         // Prevent example from ending if CTL+C is pressed.
+
+
+        string side = Console.ReadLine() == "1" ? "<P1>" : "<P2>";
+        ConsoleKeyInfo cki;
+
         Console.TreatControlCAsInput = true;
 
         Console.WriteLine("Press any combination of CTL, ALT, and SHIFT, and a console key.");
         Console.WriteLine("Press the Escape (Esc) key to quit: \n");
-                    do
-                    {
-                        cki = Console.ReadKey();
-                        Console.Write(" --- You pressed ");
-                        Console.WriteLine(cki.Key.ToString());
 
-                        if (cki.KeyChar == 'k')
-                        {
-                            messageSent = Encoding.ASCII.GetBytes("S<EOF>");
-                            byteSent = sender.Send(messageSent);
-                            continue;
-                        }
+        
 
-                        messageSent = Encoding.ASCII.GetBytes(" --- You pressed " + cki.Key.ToString() + "<EOF>");
-                        byteSent = sender.Send(messageSent);
-            
-            
+        do
+        {
+            cki = Console.ReadKey();
+            Console.Write(" --- You pressed ");
+            Console.WriteLine(cki.Key.ToString());
+
+            /*if (cki.KeyChar == 'k')
+            {
+                byte[] msg = Encoding.ASCII.GetBytes("S<EOF>");
+                udpc.Send(msg, msg.Length);
+                continue;
+            }*/
+
+
+            // Data to send
+            byte[] msg = Encoding.ASCII.GetBytes(side + cki.Key.ToString());
+            udpc.Send(msg, msg.Length);
+
+            // received Data
+            /*byte[] rdata = udpc.Receive(ref ep);
+            string job = Encoding.ASCII.GetString(rdata);
+            Console.WriteLine(job);*/
         } while (cki.KeyChar != 'k');
-
-            // Data buffer
-                    byte[] messageReceived = new byte[1024];
-
-            // We receive the message using 
-            // the method Receive(). This 
-            // method returns number of bytes
-            // received, that we'll use to 
-            // convert them to string
-            int byteRecv = sender.Receive(messageReceived);
-            Console.WriteLine("Message from Server -> {0}", 
-                  Encoding.ASCII.GetString(messageReceived, 
-                                             0, byteRecv));
-
-
-
-            Console.ReadKey();
-
-            // Close Socket using 
-                    // the method Close()
-                    sender.Shutdown(SocketShutdown.Both);
-            sender.Close();
-        }
-        
-        // Manage of Socket's Exceptions
-        catch (ArgumentNullException ane) {
-            
-            Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-        }
-        
-        catch (SocketException se) {
-            
-            Console.WriteLine("SocketException : {0}", se.ToString());
-        }
-        
-        catch (Exception e) {
-            Console.WriteLine("Unexpected exception : {0}", e.ToString());
-        }
     }
-    
-    catch (Exception e) {
-        
-        Console.WriteLine(e.ToString());
-    }
-}
-}
 }
--- ServerSide/src/Server.cs
+++ ServerSide/src/Server.cs
@@ -1,135 +1,31 @@
-// A C# Program for Server
+// Server-Side Implementation Of UDP:
 using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Configuration;
 
-namespace Server {
-
-class Program {
-
-// Main Method
-static void Main(string[] args)
+class StudentUDPServer
 {
-    ExecuteServer();
-}
-
-public static void ExecuteServer()
-{
-    // Establish the local endpoint 
-    // for the socket. Dns.GetHostName
-    // returns the name of the host 
-    // running the application.
-    IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
-    IPAddress ipAddr = ipHost.AddressList[0];
-    IPAddress myIp = IPAddress.Parse("127.0.0.1");
-    IPEndPoint localEndPoint = new IPEndPoint(myIp, 25565);
-
-    // Creation TCP/IP Socket using 
-    // Socket Class Constructor
-    Socket listener = new Socket(myIp.AddressFamily,
-                 SocketType.Stream, ProtocolType.Tcp);
-
-    try {
-        
-        // Using Bind() method we associate a
-        // network address to the Server Socket
-        // All client that will connect to this 
-        // Server Socket must know this network
-        // Address
-        listener.Bind(localEndPoint);
-
-        // Using Listen() method we create 
-        // the Client list that will want
-        // to connect to Server
-        listener.Listen(10);
-
-        while (true) {
-            
-            Console.WriteLine("Waiting connection ... ");
-
-            // Suspend while waiting for
-            // incoming connection Using 
-            // Accept() method the server 
-            // will accept connection of client
-            Socket clientSocket = listener.Accept();
-
-            // Data buffer
-            byte[] bytes = new Byte[1024];
-            string data = null;
-
-            while (true) {
-
-                int numByte = clientSocket.Receive(bytes);
-                
-                data += Encoding.ASCII.GetString(bytes,
-                                           0, numByte);
-                                           
-                if (data.IndexOf("<EOF>") > -1)
-                    break;
-            }
-
-            Console.WriteLine("Text received -> {0} ", data);
-            byte[] message = Encoding.ASCII.GetBytes("Test Server");
-
-            // Send a message to Client 
-            // using Send() method
-            clientSocket.Send(message);
-                    int i = 0;
-                    do
-                    {
-                        i++;
-                        data = "";
-                        while (true)
-                        {
-
-                            int numByte = clientSocket.Receive(bytes);
-
-                            data += Encoding.ASCII.GetString(bytes,
-                                                       0, numByte);
-
-                            if (data.IndexOf("<EOF>") > -1)
-                                break;
-                        }
-                Console.WriteLine(data);
-                    }
-                    while (data != "S<EOF>");
-        
-            // Close client Socket using the
-            // Close() method. After closing,
-            // we can use the closed Socket 
-            // for a new Client Connection
-            clientSocket.Shutdown(SocketShutdown.Both);
-            clientSocket.Close();
-        }
-    }
-    
-    catch (Exception e) {
-        Console.WriteLine(e.ToString());
-    }
-}
-}
-}
-/*
     public static void Main()
     {
-        ConsoleKeyInfo cki;
-        // Prevent example from ending if CTL+C is pressed.
-        Console.TreatControlCAsInput = true;
+        UdpClient udpc = new UdpClient(25565);
+        Console.WriteLine("Server Started, servicing on port no. 25565");
+        IPEndPoint ep = null;
 
-        Console.WriteLine("Press any combination of CTL, ALT, and SHIFT, and a console key.");
-        Console.WriteLine("Press the Escape (Esc) key to quit: \n");
-        do
+        while (true)
         {
-            cki = Console.ReadKey();
-            Console.Write(" --- You pressed ");
-            if ((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
-            if ((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
-            if ((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+");
-            if (cki.Key.ToString() == "d")
-                Console.WriteLine("1");
-            Console.WriteLine(cki.Key.ToString());
-            
-        } while (cki.Key != ConsoleKey.Escape);
+
+            // Store received data from client
+            byte[] receivedData = udpc.Receive(ref ep);
+
+            string studentName = Encoding.ASCII.GetString(receivedData);
+
+            //string msg = ConfigurationSettings.AppSettings[studentName];
+            /*if (msg == null) msg = "No such Student available for conversation";
+            byte[] sdata = Encoding.ASCII.GetBytes(msg);
+            udpc.Send(sdata, sdata.Length, ep);*/
+            Console.WriteLine(studentName);
+        }
     }
-*/
+}
