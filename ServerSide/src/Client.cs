289ed2fa2355e4abf2a0b8ce7f03fbfa216eb540
Kiliken <francescopaolomariani7@gmail.com>
2025-06-07 18:31:25 +0900

    Client/Server Sync

--- ServerSide/src/Client.cs
+++ ServerSide/src/Client.cs
@@ -8,27 +8,31 @@ class StudentUDPClient
 {
     public static void Main(string[] args)
     {
-        UdpClient udpc = new UdpClient("127.0.0.1", 25565);
+        Console.WriteLine("IP ADDRESS: ");
+        string ip = Console.ReadLine();
+        UdpClient udpc = new UdpClient(ip, 25565);
         IPEndPoint ep = null;
-        
+
+        string message;
+
         // Prevent example from ending if CTL+C is pressed.
 
-
+        Console.WriteLine("PLAYER SIDE: ");
         string side = Console.ReadLine() == "1" ? "<P1>" : "<P2>";
         ConsoleKeyInfo cki;
 
-        Console.TreatControlCAsInput = true;
+        //Console.TreatControlCAsInput = true;
 
         Console.WriteLine("Press any combination of CTL, ALT, and SHIFT, and a console key.");
         Console.WriteLine("Press the Escape (Esc) key to quit: \n");
 
-        
+
 
         do
         {
             cki = Console.ReadKey();
-            Console.Write(" --- You pressed ");
-            Console.WriteLine(cki.Key.ToString());
+            //Console.Write(" --- You pressed ");
+            //Console.WriteLine(cki.Key.ToString());
 
             /*if (cki.KeyChar == 'k')
             {
@@ -43,9 +47,16 @@ class StudentUDPClient
             udpc.Send(msg, msg.Length);
 
             // received Data
-            /*byte[] rdata = udpc.Receive(ref ep);
-            string job = Encoding.ASCII.GetString(rdata);
-            Console.WriteLine(job);*/
-        } while (cki.KeyChar != 'k');
+            byte[] rdata = udpc.Receive(ref ep);
+            message = Encoding.ASCII.GetString(rdata);
+            Console.WriteLine(message);
+
+            //if (message.Substring(0, 4) != side)
+            {
+                //Console.WriteLine(message);
+            }
+
+        }while (cki.KeyChar != 'k');
+
     }
 }
